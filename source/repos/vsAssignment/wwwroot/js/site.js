ec2316504d8c5aebe11675295ca72c35fa691979
bjlesiguez <136043137+bjlesiguez@users.noreply.github.com>
2024-04-15 17:21:40 -0600

    new

---
+++ source/repos/vsAssignment/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+// Add simple animation to the h1 element
+const heading = document.querySelector("h1");
+heading.addEventListener("mouseover", () => {
+    heading.style.color = "red";
+});
+heading.addEventListener("mouseout", () => {
+    heading.style.color = "#333";
+});
